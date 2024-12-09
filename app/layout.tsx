f99d8853f3f1dd6c1b449146e2415843ad1ff8eb
Kashaf Naz <kashafnazk389@gmail.com>
2024-12-09 21:49:16 +0500

    UI/UX hackathon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import "./globals.css";
+import Header from "./header/page";
+import Footer from "./footer/page";
 
 function RootLayout({children}:any){
   return(
     <html lang="en">
       <body>
+        <Header/>
         
         {children}
+        <Footer />
         
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
 import Image from "next/image";
 
 export default function Home() {
@@ -6,7 +8,7 @@ export default function Home() {
       <div className="flex p-[80px] gap-[40px] w-[90%] mx-auto bg-[#F0F2F3] rounded-bl-[48px]  rounded-br-[48px]">
         <div>
           <p className="font-[sans] uppercase">wellcome to Chairy</p>
-          <h1 className="font-[sans-serif ] text-[#272343] font-bold text-[60px] pt-[20px] pb-[20px]">Best Furniture
+          <h1 className="font-[sans-serif ] text-[#272343] font-bold text-[60px]  pt-[20px] pb-[20px]">Best Furniture
             Collection for your
             interior.</h1>
             <button className="flex gap-2 bg-[#029FAE] text-center text-[#FFFFFF] rounded-[8px] h-[52px] w-[171px] px-[24px] py-[14px]">Shop Now
@@ -26,12 +28,12 @@ export default function Home() {
           width={438}
           height={584}
           priority
-          className="w-[534px] h-[484px]" 
+          className="w-[534px] h-[484px] " 
           />
         </div>
       </div>
 
-      <div className="flex gap-[32px] mt-[20px] pl-[10px] pr-[10px] h-[139px] w-[90%] mx-auto">
+      <div className="flex gap-[32px] lg:gap-[45px] xl:gap-[85px] mt-[20px] pl-[10px] pr-[10px] h-[139px] w-[90%] mx-auto">
         <Image src="/zapeirlogo.png"
          alt="companiesLogo"
          width={85}
@@ -51,7 +53,7 @@ export default function Home() {
         <Image src="/logo4.png" 
          alt="companiesLogo"
          width={63}
-         height={59}
+         height={63}
          priority  />
         <Image src="/burntoast.png" 
          alt="companiesLogo"
@@ -70,39 +72,115 @@ export default function Home() {
          priority />
       </div>
       <div className="w-[90%] mx-auto">
-        <h2 className="text-[34px] mb-4">Featured Products</h2>
-        <div className="flex gap-2  ">
-          <Image
-           src="/chair1.png"
-            alt="Featured Products chairs"
-            width={212}
-            height={212} 
-            className="rounded-[6px]"
-            priority />
-          <Image
-           src="/chair2.png" 
-          alt="Featured Products chairs"
-           width={212} 
-           height={212} 
-           className="rounded-[10px] "
-           priority/>
-          <Image 
-          src="/chair3.png" 
-          alt="Featured Products chairs" 
-          width={212} 
-          height={212} 
-          className="rounded-[10px] " 
-          priority/>
-          <Image 
-          src="/chair4.png" 
-          alt="Featured Products chairs" 
-          width={212} 
-          height={212} 
-          className="rounded-[6px]"
-          priority />
+  <h2 className="text-[34px] mb-6 mt-4">Featured Products</h2>
+  <div className="grid  gap-2 grid-cols-2 xl:grid-cols-4 mb-6">
+    
+    {/* Product 1 */}
+    <div className="relative w-[312px] h-[377px]">
+      <Image
+        src="/chair1.png"
+        alt="Featured Products chairs"
+        width={312}
+        height={312}
+        className="rounded-[6px]"
+        priority
+      />
+      <button className="absolute top-2 left-2 bg-[#01AD5A] text-white px-2 py-1 text-xs rounded">
+        New
+      </button>
+      <div className="flex justify-between items-center mt-4 px-2">
+        <div>
+          <h6 className="text-lg font-semibold hover:text-[#007580] cursor-pointer">
+            Library Stool Chair
+          </h6>
+          <p className="text-gray-600 font-medium">$20</p>
         </div>
+        <button className="w-[50px] h-[50px] rounded-[5px] bg-[#F0F2F3] text-black hover:bg-[#029FAE] flex items-center justify-center">
+        <FontAwesomeIcon icon={faCartShopping} className='w-[22px] h-[22px]'/> 
+        </button>
       </div>
-
     </div>
+
+    {/* Product 2 */}
+    <div className="relative w-[312px] h-[377px]">
+      <Image
+        src="/chair2.png"
+        alt="Featured Products chairs"
+        width={312}
+        height={312}
+        className="rounded-[6px]"
+        priority
+      />
+      <button className="absolute top-2 left-2 bg-[#01AD5A] text-white px-2 py-1 text-xs rounded">
+        New
+      </button>
+      <div className="flex justify-between items-center mt-4 px-2">
+        <div>
+          <h6 className="text-lg font-semibold hover:text-[#007580] cursor-pointer">
+            Library Stool Chair
+          </h6>
+          <p className="text-gray-600 font-medium">$20</p>
+        </div>
+        <button className="w-[50px] h-[50px] rounded-[5px] bg-[#F0F2F3] text-black hover:bg-[#029FAE] flex items-center justify-center">
+        <FontAwesomeIcon icon={faCartShopping} className='w-[22px] h-[22px]'/> 
+        </button>
+      </div>
+    </div>
+
+    {/* Product 3 */}
+    <div className="relative w-[312px] h-[377px]">
+      <Image
+        src="/chair3.png"
+        alt="Featured Products chairs"
+        width={312}
+        height={312}
+        className="rounded-[6px]"
+        priority
+      />
+      <button className="absolute top-2 left-2 bg-[#01AD5A] text-white px-2 py-1 text-xs rounded">
+        New
+      </button>
+      <div className="flex justify-between items-center mt-4 px-2">
+        <div>
+          <h6 className="text-lg font-semibold hover:text-[#007580] cursor-pointer">
+            Library Stool Chair
+          </h6>
+          <p className="text-gray-600 font-medium">$20</p>
+        </div>
+        <button className="w-[50px] h-[50px] rounded-[5px] bg-[#F0F2F3] text-black hover:bg-[#029FAE] flex items-center justify-center">
+        <FontAwesomeIcon icon={faCartShopping} className='w-[22px] h-[22px]'/> 
+        </button>
+      </div>
+    </div>
+
+    {/* Product 4 */}
+    <div className="relative w-[312px] h-[377px]">
+      <Image
+        src="/chair4.png"
+        alt="Featured Products chairs"
+        width={312}
+        height={312}
+        className="rounded-[6px]"
+        priority
+      />
+      <button className="absolute top-2 left-2 bg-[#01AD5A] text-white px-2 py-1 text-xs rounded">
+        New
+      </button>
+      <div className="flex justify-between items-center mt-4 px-2">
+        <div>
+          <h6 className="text-lg font-semibold hover:text-[#007580] cursor-pointer">
+            Library Stool Chair
+          </h6>
+          <p className="text-gray-600 font-medium">$20</p>
+        </div>
+        <button className="w-[50px] h-[50px] rounded-[5px] bg-[#F0F2F3] text-black hover:bg-[#029FAE] flex items-center justify-center">
+        <FontAwesomeIcon icon={faCartShopping} className='w-[22px] h-[22px]'/> 
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  </div>
   );
 }
